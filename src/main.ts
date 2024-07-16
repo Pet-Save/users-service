93ec23d5fb6f5c4d70e65d6093f502d5949e863f
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-07-16 13:00:41 -0400

    feat: config set up

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';    // IMPORTING LOGGER
+
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<string>('PORT') || 3000;
+  const logger = new Logger();             // CREATING INSTANCE OF LOGGER
+  
+  await app.listen(PORT);
+  logger.log(`Application running on port ${PORT}`); // LOGGER OF TYPE LOG
+
 }
 bootstrap();
