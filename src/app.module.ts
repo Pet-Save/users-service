1d8d60406a321852255eae89a71ac5664bb3c3ef
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-07-16 14:05:24 -0400

    feat: typeorm setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { FormsModule } from './forms/forms.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+const SnakeNamingStrategy = require('typeorm-naming-strategies').SnakeNamingStrategy;
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => {
+        console.log();
+        return ({
+          type: config.get('DB_TYPE'), // type of our database
+          host: config.get('DB_HOST'), // database host
+          port: config.get('DB_PORT'), // database host
+          username: config.get('DB_USERNAME'), // username
+          password: config.get('DB_PASSWORD'), // user password
+          database: config.get('DB_DATABASE'), // name of our database,
+          autoLoadEntities: true, // models will be loaded automatically 
+          namingStrategy: new SnakeNamingStrategy(),
+        } as TypeOrmModuleOptions);
+      },
+      inject: [ConfigService],
+    }),
+    FormsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
