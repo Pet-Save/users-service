55e33c0498f4242150ad0edf0586096b1c8392e5
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-07-21 00:23:21 -0400

    feat: mikroOrm generated files

---
+++ src/db/migrations/Migration20240721041629.ts
@@ -0,0 +1,9 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20240721041629 extends Migration {
+
+  async up(): Promise<void> {
+    this.addSql('create table "contact_us_form" ("id" serial primary key, "created_at" timestamptz not null, "updated_at" timestamptz not null, "created_by" varchar(255) not null, "updated_by" varchar(255) not null, "first_name" varchar(255) not null, "last_name" varchar(255) not null, "email" varchar(255) not null, "message" varchar(255) not null);');
+  }
+
+}
