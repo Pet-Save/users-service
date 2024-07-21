73438e194cf063db8a031b84d0f969b3246a8393
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-07-21 11:53:57 -0400

    feat: constructor to call

--- src/forms/entities/contact-use-form.entity.ts
+++ src/forms/entities/contact-use-form.entity.ts
@@ -1,12 +1,10 @@
-import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+import { Entity, PrimaryKey, Property } from '@mikro-orm/postgresql';
 import { BaseEntity } from '../../db/base.entity';
+import { CreateContactUsFormDto } from '../dto/create-contact-us-form.dto';
 
 
 @Entity()
 export class ContactUsForm extends BaseEntity {
-  @PrimaryKey()
-  id!: number;
-
   @Property()
   firstName: string;
 
@@ -18,4 +16,14 @@ export class ContactUsForm extends BaseEntity {
 
   @Property()
   message: string;
+
+  constructor({ firstName, lastName, email, message }: CreateContactUsFormDto) {
+    super();
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.message = message;
+    this.createdBy = email;
+    this.updatedBy = email;
+  }
 }
