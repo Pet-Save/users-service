f45fec2517fe358da8d5a24483e8c1269300f2fa
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-07-21 10:01:03 -0400

    feat: implemented down method

--- src/db/migrations/Migration20240721041629.ts
+++ src/db/migrations/Migration20240721041629.ts
@@ -6,4 +6,8 @@ export class Migration20240721041629 extends Migration {
     this.addSql('create table "contact_us_form" ("id" serial primary key, "created_at" timestamptz not null, "updated_at" timestamptz not null, "created_by" varchar(255) not null, "updated_by" varchar(255) not null, "first_name" varchar(255) not null, "last_name" varchar(255) not null, "email" varchar(255) not null, "message" varchar(255) not null);');
   }
 
+  async down(): Promise<void> {
+    this.addSql('DROP TABLE "contact_us_form"');
+  }
+
 }
