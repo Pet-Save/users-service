909fbc9a188b29ed81e7fe75528ff92248887f4f
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-07-27 10:33:58 -0400

    feat: dto for volunteer application form

---
+++ src/forms/dto/create-volunteer-application-form.dto.ts
@@ -0,0 +1,101 @@
+import { Type } from 'class-transformer';
+import { IsString, IsEmail, IsNotEmpty, IsNotEmptyObject, IsBoolean, IsArray, IsInt, IsOptional, ValidateNested } from 'class-validator';
+import { IsAtLeastOneFieldDefined } from 'src/common/decorators/isAtLeastOneFieldDefined';
+
+class Timetable {
+    @IsOptional()
+    @IsArray()
+    @IsBoolean({ each: true })
+    monday?: boolean[];
+
+    @IsOptional()
+    @IsArray()
+    @IsBoolean({ each: true })
+    tuesday: boolean[];
+
+    @IsOptional()
+    @IsArray()
+    @IsBoolean({ each: true })
+    wednesday: boolean[];
+
+    @IsOptional()
+    @IsArray()
+    @IsBoolean({ each: true })
+    thursday: boolean[];
+
+    @IsOptional()
+    @IsArray()
+    @IsBoolean({ each: true })
+    friday: boolean[];
+
+    @IsOptional()
+    @IsArray()
+    @IsBoolean({ each: true })
+    saturday: boolean[];
+
+    @IsOptional()
+    @IsArray()
+    @IsBoolean({ each: true })
+    sunday: boolean[]
+}
+
+export class CreateVolunteerApplicationFormDto {
+    @IsString()
+    @IsNotEmpty()
+    firstName: string;
+
+    @IsString()
+    @IsNotEmpty()
+    lastName: string;
+
+    @IsString()
+    @IsNotEmpty()
+    phoneNumber: string
+
+    @IsString()
+    @IsNotEmpty()
+    address: string
+
+    @IsString()
+    @IsNotEmpty()
+    city: string
+
+    @IsString()
+    @IsNotEmpty()
+    postalCode: string
+
+    @IsString()
+    @IsNotEmpty()
+    occupation: string
+
+    @IsEmail()
+    @IsNotEmpty()
+    email: string
+
+    @IsInt()
+    @IsNotEmpty()
+    @Type(() => Number)
+    age: number
+
+    @IsString()
+    @IsNotEmpty()
+    emergencyContactName: string;
+
+    @IsString()
+    @IsNotEmpty()
+    emergencyContactPhoneNumber: string;
+
+    @IsNotEmptyObject()
+    @IsAtLeastOneFieldDefined([
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday",
+        "sunday",
+    ])
+    @Type(() => Timetable)
+    @ValidateNested()
+    timetable!: Timetable;
+}
