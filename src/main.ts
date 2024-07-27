53253fef35dcb51cfab8200f21ef06af65391200
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-07-27 14:49:33 -0400

    feat: added a global api prefix

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { Logger, ValidationPipe } from '@nestjs/common';    // IMPORTING LOGGER
+import { Logger, RequestMethod, ValidationPipe } from '@nestjs/common';    // IMPORTING LOGGER
 
 import * as dotenv from 'dotenv';
 dotenv.config();
@@ -13,6 +13,9 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
   }));
+  app.setGlobalPrefix('api/v1', {
+    exclude: [{ path: 'health', method: RequestMethod.GET }],
+  });
   const logger = new Logger();             // CREATING INSTANCE OF LOGGER
   
   await app.listen(PORT);
