93e80fc51aa70cfddff0d611b3060cb1537f53fe
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-07-30 12:20:09 -0400

    feat: injected Setting module to forms module to get settings service method

--- src/forms/forms.module.ts
+++ src/forms/forms.module.ts
@@ -3,10 +3,12 @@ import { FormsService } from './forms.service';
 import { FormsController } from './forms.controller';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { ContactUsForm } from './entities/contact-use-form.entity';
+import { SettingsModule } from 'src/settings/settings.module';
 
 @Module({
   imports: [
     MikroOrmModule.forFeature([ContactUsForm]),
+    SettingsModule
   ],
   controllers: [FormsController],
   providers: [FormsService],
