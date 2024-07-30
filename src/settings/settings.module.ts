ec6cd35dd6723a45f2059b6ed8eeeae20d4dba65
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-07-30 12:17:12 -0400

    feat: injected cache manager and methods for time sessions

--- src/settings/settings.module.ts
+++ src/settings/settings.module.ts
@@ -3,12 +3,19 @@ import { SettingsService } from './settings.service';
 import { SettingsController } from './settings.controller';
 import { TimeSessions } from './entities/time-sessions.entity';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
-    MikroOrmModule.forFeature([TimeSessions]),
+    MikroOrmModule.forFeature([
+      TimeSessions,
+    ]),
+    CacheModule.register()
   ],
   controllers: [SettingsController],
   providers: [SettingsService],
+  exports: [
+    SettingsService,
+  ]
 })
 export class SettingsModule {}
--- src/settings/settings.service.ts
+++ src/settings/settings.service.ts
@@ -1,12 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateTimeSessionDto } from './dto/create-time-session.dto';
 import { EntityManager, IDatabaseDriver, Connection, MikroORM } from '@mikro-orm/core';
 import { TimeSessions } from './entities/time-sessions.entity';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
+import CACHE_KEYS from '../common/cache-keys';
+import CACHE_TIMES from '../common/cache-times';
 
 @Injectable()
 export class SettingsService {
   em: EntityManager<IDatabaseDriver<Connection>>;
   constructor(
+    @Inject(CACHE_MANAGER)
+    private cacheManager: Cache,
     private readonly orm: MikroORM,
   ) {
     this.em = this.orm.em.fork();
@@ -14,15 +20,26 @@ export class SettingsService {
 
   getTimeSession(id: number) {
     try{
-      return this.em.findOne(TimeSessions, id);
+      return this.em.findOneOrFail(TimeSessions, id);
     } catch(e) {
       throw e
     }
   }
 
-  getTimeSessions() {
+  async getTimeSessions(): Promise<Map<number, TimeSessions>> {
     try{
-      return this.em.findAll(TimeSessions);
+      let sessions = await this.cacheManager.get(CACHE_KEYS.TIME_SESSIONS) as Map<number, TimeSessions>;
+      if (!sessions) {
+        sessions = (await this.em.findAll(
+          TimeSessions,
+          { populate: undefined, }
+        )).reduce((acc,curr) => {
+          acc.set(curr.id, curr);
+          return acc
+        }, new Map<number, TimeSessions>());
+        await this.cacheManager.set(CACHE_KEYS.TIME_SESSIONS, sessions, CACHE_TIMES.ONE_MINUTE);
+      }
+      return sessions
     } catch(e) {
       throw e
     }
