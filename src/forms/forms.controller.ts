2a3df36803ccf2ee7b75c214b0ac31d00545eb97
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-07-30 12:20:57 -0400

    feat: basic endpoint for posting various forms with try catch block returing BadGateway Exception

--- src/forms/forms.controller.ts
+++ src/forms/forms.controller.ts
@@ -1,8 +1,9 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, BadGatewayException } from '@nestjs/common';
 import { FormsService } from './forms.service';
-import { CreateContactUsFormDto, CreateFormDto } from './dto/create-contact-us-form.dto';
+import { CreateContactUsFormDto } from './dto/create-contact-us-form.dto';
 import { UpdateFormDto } from './dto/update-form.dto';
-
+import { CreatePetFormDto } from './dto/create-pet-application-form.dto';
+import { CreateVolunteerApplicationFormDto } from './dto/create-volunteer-application-form.dto';
 
 @Controller('forms')
 export class FormsController {
@@ -13,16 +14,42 @@ export class FormsController {
     try {
       return this.formsService.createContactUsForm(createContactUsFormDto);
     } catch (e) {
-      throw new BadGatewayException();
+      return new BadGatewayException(e);
     }
   }
+
   @Post('volunteer-applications')
-  createVolunteer(@Body() createFormDto: CreateFormDto) {
-    return this.formsService.create(createFormDto);
+  createVolunteer(@Body() createVolunteerApplicationForm: CreateVolunteerApplicationFormDto) {
+    try {
+      return this.formsService.createVolunteerApplicationForm(createVolunteerApplicationForm);
+    } catch (e) {
+      return new BadGatewayException(e);
+    }
   }
+  
+
   @Post('pets-applications')
-  createPets(@Body() createFormDto: CreateFormDto) {
-    return this.formsService.create(createFormDto);
+  createPets(
+    @Body() createFormDto: CreatePetFormDto
+  ) {
+    try {
+      let res = null;
+      switch(true) {
+        case("adoptPets" in createFormDto): {
+          res = this.formsService.createPetAdoptionForm(createFormDto)
+          break;
+        }
+        case("fosterTypes" in createFormDto): {
+          break;
+  
+        }
+        default: {
+          break;
+        }
+      }
+    } catch(e) {
+      return new BadGatewayException(e);
+    }
   }
 
   @Get()
