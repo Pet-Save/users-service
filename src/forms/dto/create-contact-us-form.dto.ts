5d7288520e13acd702ab728490aeb14d24326055
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-08-20 15:11:00 -0400

    feat: added length check

--- src/forms/dto/create-contact-us-form.dto.ts
+++ src/forms/dto/create-contact-us-form.dto.ts
@@ -1,15 +1,18 @@
-import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
+import { IsString, IsEmail, IsNotEmpty, Length } from 'class-validator';
 
 export class CreateContactUsFormDto {
     @IsString()
+    @Length(1, 20)
     @IsNotEmpty()
     firstName: string;
 
     @IsString()
+    @Length(1, 20)
     @IsNotEmpty()
     lastName: string;
 
     @IsEmail()
+    @Length(1, 100)
     @IsNotEmpty()
     email: string;
 
