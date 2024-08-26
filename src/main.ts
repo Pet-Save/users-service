4e52c6ccfbbadb23aeb3925588a262d369a09ce3
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-08-26 15:07:54 -0400

    fix: anable cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import helmet from 'helmet';
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.use(helmet());
   const configService = app.get(ConfigService);
   const PORT = configService.get<string>('PORT') || 3000;
