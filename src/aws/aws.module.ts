600b3c9b87a1b8992ae1fa13a5795521256715b5
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-08-27 00:50:20 -0400

    fix: use config service

--- src/aws/aws.module.ts
+++ src/aws/aws.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SesService } from './ses/ses.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule],
   providers: [SesService],
   exports: [
     SesService,
--- src/aws/ses/ses.service.ts
+++ src/aws/ses/ses.service.ts
@@ -1,8 +1,11 @@
 import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class SesService {
+    constructor(private configService: ConfigService) {}
+
     async sendEmail(subject: string, content: string) {
         try {
             const params = {
@@ -24,16 +27,17 @@ export class SesService {
                 },
             }
             const ses = new SESClient({
-                region: process.env.AWS_REGION,
+                region: this.configService.get('AWS_REGION') as string,
                 credentials: {
-                    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
-                    accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+                    secretAccessKey: this.configService.get('AWS_SECRET_ACCESS_KEY') as string,
+                    accessKeyId: this.configService.get('AWS_ACCESS_KEY_ID') as string,
                   }
             });
             const command = new SendEmailCommand(params);
             const sesResult = await ses.send(command);
             return sesResult;
         } catch (err) {
+            console.error(err)
             throw (err)
         }
     }
