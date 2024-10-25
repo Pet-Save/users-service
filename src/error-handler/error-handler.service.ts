3478e87aa56303d18d07ce7c4a5f9f023be0fb22
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-10-25 16:08:51 -0400

    feat: handling errors from mikroorm

--- src/error-handler/error-handler.service.ts
+++ src/error-handler/error-handler.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { ValidationException } from './errors/ValidationException';
 import { ServerException } from './errors/ServerException';
+import { ValidationError as MikroOrmError } from '@mikro-orm/postgresql';
 
 @Injectable()
 export class ErrorHandlerService {
     handleError(error: any): void {
         if (error instanceof ValidationException) throw new ValidationException(error.message, error.stack, error.queryParams, error.description)
+        else if (error instanceof MikroOrmError) {
+            throw new ValidationException(
+                error.message,
+                error.stack,
+                null,
+                error.name
+            )
+        }
         else if (error instanceof ServerException) throw new ServerException(error.message, error.stack, error.queryParams, error.description)
     }
 }
